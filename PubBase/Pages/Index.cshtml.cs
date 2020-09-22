9a9ababa87e6b29aeff3f40ce742223df8dc5a1e
Filip Plass <filip.plass@pslib.cz>
2020-09-22 15:00:09 +0200

    3

--- PubBase/Pages/Index.cshtml.cs
+++ PubBase/Pages/Index.cshtml.cs
@@ -5,21 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using PubBase.Models;
 
 namespace PubBase.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        AppDbContext _db { get; set; }
+        public List<Pub> Pubs { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
-
+            Pubs = _db.Pubs.ToList();
         }
     }
 }
