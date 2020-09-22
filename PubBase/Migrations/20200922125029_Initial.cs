904101253949b4afa9a119628efe034ede93ae26
Filip Plass <filip.plass@pslib.cz>
2020-09-22 14:52:54 +0200

    frst

---
+++ PubBase/Migrations/20200922125029_Initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PubBase.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Municipality = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pubs", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pubs",
+                columns: new[] { "Id", "Municipality", "Name" },
+                values: new object[] { 1, "Liberec", "Svah" });
+
+            migrationBuilder.InsertData(
+                table: "Pubs",
+                columns: new[] { "Id", "Municipality", "Name" },
+                values: new object[] { 2, "Jablonec nad Nisou", "Techtle" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pubs");
+        }
+    }
+}
--- PubBase/Models/AppDbContext.cs
+++ PubBase/Models/AppDbContext.cs
@@ -8,5 +8,15 @@ namespace PubBase.Models
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Pub> Pubs { get;set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Pub>().HasData(new Pub { Id = 1, Name = "Svah", Municipality = "Liberec"});
+            builder.Entity<Pub>().HasData(new Pub { Id = 2, Name = "Techtle", Municipality = "Jablonec nad Nisou" });
+        }
     }
 }
