17dc90608e6a207741701f2f8c1871055bddb914
Filip Plass <filip.plass@pslib.cz>
2020-09-25 11:41:23 +0200

    HW

---
+++ PubBase/Pages/AddPub.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PubBase.Models;
+
+namespace PubBase.Pages
+{
+    public class AddPubModel : PageModel
+    {
+        AppDbContext _db { get; set; }
+        public List<Pub> Pubs { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Muni { get; set; }
+        public AddPubModel(AppDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPost()
+        {
+            _db.Pubs.Add(new Pub { Name = Name, Municipality = Muni });
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
